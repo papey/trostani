e20e61d6a27adf92ac4d30098ff3550e4fd35285
Wilfried OLLIVIER <wollivier@fdn.fr>
2020-01-23 13:35:10 +0100

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import * as yargs from "yargs";
 // Args and cli setup
 let args = yargs
   .scriptName("Trostani")
-  .version("0.1.1")
+  .version("0.1.1-3")
   .help()
   .option("config", {
     alias: "c",
