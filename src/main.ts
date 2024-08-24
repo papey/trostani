be93757bef412a3e3e310b23d1a3978e0c6b7359
Wilfried OLLIVIER <wollivier@fdn.fr>
2024-08-24 17:08:25 +0200

    chore: Bump version to 0.4.24

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import * as yargs from "yargs";
 // Args and cli setup
 let args = yargs
   .scriptName("Trostani")
-  .version("0.4.23")
+  .version("0.4.24")
   .help()
   .option("config", {
     alias: "c",
